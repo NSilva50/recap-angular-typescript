2daf6c2e258eb3ca8aa9fcb63e6421a6b6ce5ef6
User <nathansilva50@gmail.com>
2018-06-07 11:40:44 -0700

    Hopes for improvment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,26 @@
 import { Component } from '@angular/core';
 
+class Friend {
+  fname: string;
+  lname: string;
+  age: number;
+  gender: string;
+
+  constructor(
+    fname: string,
+    lname: string,
+    age: number,
+    gender: string
+  ){
+    this.fname = fname;
+    this.lname = lname;
+    this.age = age;
+    this.gender = gender;
+  }
+}
+
+let friend1 = new Friend("Michard", "Tornado", 26, "Apache Helicopter");
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +28,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
 }
