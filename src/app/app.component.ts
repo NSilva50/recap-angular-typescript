58fe85ee6f8d001dd1ee7618e42092a4d6552ae8
User <nathansilva50@gmail.com>
2018-06-11 12:39:21 -0700

    learned hhow to use built-in directives.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  displayParagraph: boolean = false;
+
+  users: [{
+    name: 'user1',
+    age: '43'
+  },{
+    name: 'user2',
+    age: '22'
+  },{
+    name: 'user3',
+    age: '100'
+  }];
 }
